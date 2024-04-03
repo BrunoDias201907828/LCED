b27fd5127fa770298ab4fbe8169ace0175568a81
BrunoDias201907828 <up201907828@up.pt>
2024-04-03 21:23:00 +0100

    added doc about features - removed columns

--- new_analysis.py
+++ new_analysis.py
@@ -6,7 +6,7 @@ import re
 
 
 def missing_values_column(df):
-    missing_values_per_column = df.isnull().sum
+    missing_values_per_column = df.isnull().sum(axis=1)
     missing_percentage_per_column = ((missing_values_per_column / len(df)) * 100).round(2)
 
     missing_info = pd.concat([missing_values_per_column, missing_percentage_per_column], axis=1)
@@ -30,11 +30,33 @@ def get_duplicate_rows(df):
 
 
 def get_rows_with_less_null_values(df):
+
     df = df.copy()
     missing_values_per_row = df.isnull().sum(axis=1)
     df['missing_values'] = missing_values_per_row
     df = df.sort_values(by='missing_values')
+
     return df
+
+def remove_duplicated_rows(df):
+
+    df_duplicated = get_duplicate_rows(df)
+    distinct_values_list = df_duplicated['Cod_do_Material'].unique().tolist()
+    new_df = pd.DataFrame()
+
+    for cod in distinct_values_list:
+        less_null = get_rows_with_less_null_values(df_duplicated[df_duplicated['Cod_do_Material'] == cod])
+        new_df = pd.concat([new_df, less_null.iloc[[0]]])  
+
+    df_keep = new_df.drop(columns=['missing_values'])
+
+    merged_df = pd.merge(df_duplicated, df_keep, how='outer', indicator=True)
+    df_to_drop = merged_df[merged_df['_merge'] == 'left_only'].drop(columns=['_merge'])
+
+    merged_df2 = pd.merge(df, df_to_drop, how = 'outer', indicator=True)
+    df_distinct = merged_df2[merged_df2['_merge'] == 'left_only'].drop(columns=['_merge'])
+
+    return df_distinct
 
 def convert_value_to_double(value):
     if pd.isnull(value):
@@ -72,55 +94,59 @@ def convert_to_int(df, columns):
         df[column] = df[column].apply(convert_value_to_int)
     return df
 
-df = pd.read_csv("ListaEBs.csv")
+def print_value_counts(df):
+    for column in df.columns:
+        print(f"Column: {column}")
+        print(df[column].value_counts())
+        print("\n")
 
-df_duplicated = get_duplicate_rows(df)
 
-distinct_values_list = df_duplicated['Cod_do_Material'].unique().tolist()
+def drop_columns(df):
+    columns_to_remove = ['NUMERO_DE_FASES', 'PROCESSO_FABRICACAO', 'NIVEL_REND_EFICIENCIA_-_EFF', 'Cod_do_Material', 'COD_MATERIAL_FIO_01_ENROL_01', 'CODIGO_DESENHO_ESTATOR_COMPLET', 'NUMERO_DESENHO', 'COD_DESENHO_DISCO_ROTOR', 'COD_DESENHO_DISCO_ESTATOR']
+    df = df.drop(columns_to_remove, axis=1)
+    return df
 
-new_df = pd.DataFrame()
+if __name__ == '__main__':
 
-for cod in distinct_values_list:
-    less_null = get_rows_with_less_null_values(df_duplicated[df_duplicated['Cod_do_Material'] == cod])
-    new_df = pd.concat([new_df, less_null.iloc[[0]]])
+    df = pd.read_csv("ListaEBs.csv")
+    df = remove_duplicated_rows(df)
+    total_null_values = df.isnull().sum().sum()
+    print(total_null_values)
+    df = drop_columns(df)
+    total_null_values = df.isnull().sum().sum()
+    print(total_null_values)
 
-#print(new_df)
-#print(df_duplicated)
 
-df_keep = new_df.drop(columns=['missing_values'])
-#print(df_keep)
 
-#rows ro remove
-merged_df = pd.merge(df_duplicated, df_keep, how='outer', indicator=True)
-df_to_drop = merged_df[merged_df['_merge'] == 'left_only'].drop(columns=['_merge'])
 
-#df without duplicates
-merged_df2 = pd.merge(df, df_to_drop, how = 'outer', indicator=True)
-df_distinct = merged_df2[merged_df2['_merge'] == 'left_only'].drop(columns=['_merge'])
-#print(df_distinct)
 
-distinct_value_counts = df_distinct.nunique()
-#print(distinct_value_counts)
 
-column_types = df_distinct.dtypes
-#print(column_types)
 
-columns_to_double = ['DIAMETRO_ANEL_CURTO', 'DIAMETRO_EXTERNO_ESTATOR','DIAMETRO_USINADO_ROTOR','INCLINACAO_ROTOR',
-                      'LARGURA_ANEL_CURTO','COMPRIMENTO_TOTAL_PACOTE']
 
-columns_to_int = ['BITOLA_CABO_ATERRAMEN_CAR CACA','BITOLA_CABOS_DE_LIGACAO']
 
-df_aux = convert_to_int(df_distinct, columns_to_int)
-df_converted = convert_to_double(df_aux, columns_to_double)
-column_types = df_converted.dtypes
-#print(column_types)
 
-numeric_columns = df_converted.select_dtypes(include=['int64', 'float64'])
-numeric_columns = numeric_columns.drop(columns=['Cod_do_Material'])
-
-correlation_matrix = numeric_columns.corr()
-
-plt.figure(figsize=(12, 8))
-sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
-plt.title('Correlation Matrix of Numeric Columns')
-plt.show()
+#distinct_value_counts = df.nunique()
+##print(distinct_value_counts)
+#
+#column_types = df.dtypes
+##print(column_types)
+#
+#columns_to_double = ['DIAMETRO_ANEL_CURTO', 'DIAMETRO_EXTERNO_ESTATOR','DIAMETRO_USINADO_ROTOR','INCLINACAO_ROTOR',
+#                      'LARGURA_ANEL_CURTO','COMPRIMENTO_TOTAL_PACOTE']
+#
+#columns_to_int = ['BITOLA_CABO_ATERRAMEN_CAR CACA','BITOLA_CABOS_DE_LIGACAO']
+#
+#df_aux = convert_to_int(df, columns_to_int)
+#df_converted = convert_to_double(df_aux, columns_to_double)
+#column_types = df_converted.dtypes
+##print(column_types)
+#
+#numeric_columns = df_converted.select_dtypes(include=['int64', 'float64'])
+#numeric_columns = numeric_columns.drop(columns=['Cod_do_Material'])
+#
+#correlation_matrix = numeric_columns.corr()
+#
+#plt.figure(figsize=(12, 8))
+#sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+#plt.title('Correlation Matrix of Numeric Columns')
+#plt.show()
