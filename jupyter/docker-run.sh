f2c3d2268b28ea2a5c6a6ab970df75db27e5dd36
prft <prftavares1@gmail.com>
2024-05-03 18:32:03 +0100

    jupyter setup

--- jupyter/docker-run.sh
+++ jupyter/docker-run.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -p 8888:8888 -v ./:/home/jovyan/git-repos jupyter/datascience-notebook
+docker run -d -p 8888:8888 -v ./:/home/jovyan/work/ jupyter start-notebook.sh --NotebookApp.token=''
