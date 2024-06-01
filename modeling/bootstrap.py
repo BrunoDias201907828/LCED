24fab01c716ff4d0eacfce0fe73e4a2d5d8918b5
BrunoDias201907828 <up201907828@up.pt>
2024-06-01 20:15:50 +0100

    bootstrap with ci

--- modeling/bootstrap.py
+++ modeling/bootstrap.py
@@ -2,7 +2,7 @@ from db_connection import DBConnection
 from encoding import CATEGORICAL_COLUMNS
 from modeling.utils import KMeansTransformer
 
-from sklearn.metrics import mean_squared_error, r2_score, root_mean_squared_error
+from sklearn.metrics import mean_absolute_error, r2_score, root_mean_squared_error, mean_absolute_percentage_error
 from sklearn.utils import resample
 from sklearn.experimental import enable_halving_search_cv, enable_iterative_imputer
 from sklearn.impute import IterativeImputer
@@ -51,6 +51,13 @@ FEATURES_KMEANS = [
     "ComprimentoTotalPacote [mm]",
 ]
 
+def calculate_confidence_intervals(data, confidence_level=95):
+    lower_percentile = (100 - confidence_level) / 2
+    upper_percentile = 100 - lower_percentile
+    lower_bound = np.percentile(data, lower_percentile)
+    upper_bound = np.percentile(data, upper_percentile)
+    return lower_bound, upper_bound
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -62,10 +69,9 @@ if __name__ == "__main__":
     parser.add_argument("--run_name"                                                , type=str, help="Name of the run"         )
     parser.add_argument("--experiment_name"                                         , type=str, help="Name of the experiment"  )
     args = parser.parse_args()
-    # example:
-    # python modeling/train_script.py --model random_forest --imputation NoImputation --encoding TargetEncoding --params '{"n_estimators": [10, 100, 1000], "max_depth": [3, 5, 10]}' --run_name random_forest --experiment_name default
 
-    # python3 modeling/bootstrap.py --model random_forest --imputation NoImputation --encoding TargetEncoding --params modeling/params_teste.py --run_name random_forest --experiment_name default
+
+    # python3 modeling/bootstrap.py --model xgboost --external --encoding BinaryEncoding --params modeling/params_bootstrap.json --run_name xgboost_best --experiment_name bootstrap
 
 
     mlflow.set_tracking_uri("http://localhost:5000")
@@ -99,8 +105,9 @@ if __name__ == "__main__":
             param_grid.update({"imputer__estimator": [RandomForestRegressor(), BayesianRidge()]})
 
 
-        n_iterations = 5
-        mse_scores = []
+        n_iterations = 5000
+        mae_scores = []
+        mape_scores = []
         r2_scores = []
         rmse_scores = []
         results = []
@@ -115,38 +122,63 @@ if __name__ == "__main__":
             pipeline.fit(x_train, y_train)
             y_pred = pipeline.predict(x_test)
 
-            mse = mean_squared_error(y_test, y_pred)
+            mae = mean_absolute_error(y_test, y_pred)
+            mape = mean_absolute_percentage_error(y_test, y_pred)
             r2 = r2_score(y_test, y_pred)
             rmse = root_mean_squared_error(y_test, y_pred)
 
-            mse_scores.append(mse)
+            mae_scores.append(mae)
+            mape_scores.append(mape)
             r2_scores.append(r2)
             rmse_scores.append(rmse)
 
             results.append({
                 "iteration": i,
-                "mse": mse,
+                "mae": mae,
+                "mape": mape,
                 "r2": r2,
                 "rmse": rmse
             })
 
-            mlflow.log_metric(f"mse_bootstrap_{i}", mse)
-            mlflow.log_metric(f"r2_bootstrap_{i}", r2)
-            mlflow.log_metric(f"rmse_bootstrap_{i}", rmse)
+            # mlflow.log_metric(f"mse_bootstrap_{i}", mae)
+            # mlflow.log_metric(f"mape_bootstrap_{i}", mape)
+            # mlflow.log_metric(f"r2_bootstrap_{i}", r2)
+            # mlflow.log_metric(f"rmse_bootstrap_{i}", rmse)
+
+        mae_lower, mae_upper = calculate_confidence_intervals(mae_scores)
+        mape_lower, mape_upper = calculate_confidence_intervals(mape_scores)
+        r2_lower, r2_upper = calculate_confidence_intervals(r2_scores)
+        rmse_lower, rmse_upper = calculate_confidence_intervals(rmse_scores)
 
         mlflow.log_param("model", args.model)
         mlflow.log_param("encoding", args.encoding)
         mlflow.log_param("imputation", args.imputation)
         mlflow.log_param("params", params)
-        mlflow.log_metric("mean_mse", np.mean(mse_scores))
+        mlflow.log_metric("mean_mse", np.mean(mae_scores))
+        mlflow.log_metric("mean_mape", np.mean(mape_scores))
         mlflow.log_metric("mean_r2", np.mean(r2_scores))
         mlflow.log_metric("mean_rmse", np.mean(rmse_scores))
+
+        mlflow.log_metric("mae_ci_lower", mae_lower)
+        mlflow.log_metric("mae_ci_upper", mae_upper)
+        mlflow.log_metric("mape_ci_lower", mape_lower)
+        mlflow.log_metric("mape_ci_upper", mape_upper)
+        mlflow.log_metric("r2_ci_lower", r2_lower)
+        mlflow.log_metric("r2_ci_upper", r2_upper)
+        mlflow.log_metric("rmse_ci_lower", rmse_lower)
+        mlflow.log_metric("rmse_ci_upper", rmse_upper)
+
         mlflow.sklearn.log_model(pipeline, "model")
 
         results_df = pd.DataFrame(results)
         results_df.to_csv('results.csv', index=False)
         mlflow.log_artifact('results.csv')
 
-        print(f"Model: {args.model}, Mean MSE: {np.mean(mse_scores)}, Mean R²: {np.mean(r2_scores)}, Mean RMSE: {np.mean(rmse_scores)}")
+        print(f"Model: {args.model}, Mean MAE: {np.mean(mae_scores)}, Mean MAPE: {np.mean(mape_scores)} , Mean R²: {np.mean(r2_scores)}, Mean RMSE: {np.mean(rmse_scores)}")
+        print(f"95% CI for MAE: [{mae_lower}, {mae_upper}]")
+        print(f"95% CI for MAPE: [{mape_lower}, {mape_upper}]")
+        print(f"95% CI for R²: [{r2_lower}, {r2_upper}]")
+        print(f"95% CI for RMSE: [{rmse_lower}, {rmse_upper}]")
+
 
         mlflow.end_run()
